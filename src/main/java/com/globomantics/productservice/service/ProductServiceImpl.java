20b0fc0dfca17f279686336e0dd8cc5407d8cc1f
Fausto Ruelas <fausto@rodgers-almer.com>
2020-09-20 23:40:52 -0500

    adding ProductServiceImpl

---
+++ src/main/java/com/globomantics/productservice/service/ProductServiceImpl.java
@@ -0,0 +1,15 @@
+package com.globomantics.productservice.service;
+
+import org.springframework.stereotype.Service;
+
+import com.globomantics.productservice.model.Product;
+@Service
+public class ProductServiceImpl implements ProductService {
+
+	@Override
+	public Iterable<Product> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
