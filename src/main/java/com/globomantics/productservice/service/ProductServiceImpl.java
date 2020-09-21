09d2e5efdaab582d7333e941f561a0dd404fcdc2
Fausto Ruelas <fausto@rodgers-almer.com>
2020-09-21 00:08:51 -0500

    testGetProductByIdFound Test

--- src/main/java/com/globomantics/productservice/service/ProductServiceImpl.java
+++ src/main/java/com/globomantics/productservice/service/ProductServiceImpl.java
@@ -1,5 +1,8 @@
 package com.globomantics.productservice.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
 import com.globomantics.productservice.model.Product;
@@ -7,9 +10,34 @@ import com.globomantics.productservice.model.Product;
 public class ProductServiceImpl implements ProductService {
 
 	@Override
-	public Iterable<Product> findAll() {
+	public Optional<Product> findById(Integer id) {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
+	@Override
+	public List<Product> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean update(Product product) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Product save(Product product) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean delete(Integer id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
 
 }
